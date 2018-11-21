5946bef4f196f71452461e8976b78794859a8501
viranmalaka <malaka.14@cse.mrt.ac.lk>
2018-11-21 21:46:02 +0530

    Implement automated test scenarios

--- src/MVStoreDirect.java
+++ src/MVStoreDirect.java
@@ -1,5 +1,7 @@
 import org.h2.mvstore.*;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 import static java.lang.System.currentTimeMillis;
 
 public class MVStoreDirect {
@@ -7,30 +9,52 @@ public class MVStoreDirect {
         // open the store (in-memory if fileName is null)
         MVStore s = MVStore.open("newfile");
 
-// create/get the map named "data"
         MVMap<Integer, String> map = s.openMap("data");
 
-// add and read some data
-        for(int i=1;i<1001;i++){
-            map.put(i*2, Integer.toString(i));
+        int[] arr = new int[]{10000, 100000, 1000000, 10000000};
+        boolean random = true;
+        int maxVal = arr[arr.length - 1];
+
+
+//        map.keySet().toArray();
+
+        for (int i = 0; i < maxVal; i++) {
+            int randomNum = ThreadLocalRandom.current().nextInt(0, maxVal);
+            map.put(i, i + "");
         }
 
-        int index = 990;
 
-        long init = currentTimeMillis();
-        String st = map.get(index,"True");
-        long end = currentTimeMillis();
+        for (int lim : arr) {
+            long init = currentTimeMillis();
 
-        System.out.println("Interpolation : " + Long.toString(end-init));
+            for (int i = 0; i < lim + 1; i++) {
+                if (random) {
+                    int randomNum = ThreadLocalRandom.current().nextInt(0, maxVal);
+                    String sd = map.getInterpolate(randomNum);
+                } else {
+                    String sd = map.getInterpolate(i);
+                }
+            }
 
-        init = currentTimeMillis();
-        st = map.get(index);
-        end = currentTimeMillis();
-
-        System.out.println("Binary : " + Long.toString(end-init));
+            long end = currentTimeMillis();
+            System.out.print(lim + " -->  Interpolation: " + Long.toString(end - init) + "   ----    ");
 
 
-// close the store (this will persist changes)
+            init = currentTimeMillis();
+
+            for (int i = 0; i < lim + 1; i++) {
+                if (random) {
+                    int randomNum = ThreadLocalRandom.current().nextInt(0, maxVal);
+                    String sd = map.get(randomNum);
+                } else {
+                    String sd = map.get(i);
+                }
+            }
+
+            end = currentTimeMillis();
+            System.out.println("Binary: " + Long.toString(end - init));
+        }
+
         s.close();
     }
 }
