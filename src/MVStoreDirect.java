374ccddb1e0fcc12c7ca5a7328f8d5864ead6d06
Wishmitha <wishmitha.14@cse.mrt.ac.lk>
2018-11-22 15:02:16 +0530

    implement tests for put

--- src/MVStoreDirect.java
+++ src/MVStoreDirect.java
@@ -1,8 +1,12 @@
 import org.h2.mvstore.*;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.concurrent.ThreadLocalRandom;
 
 import static java.lang.System.currentTimeMillis;
+import static java.lang.System.setOut;
 
 public class MVStoreDirect {
     public static void main(String[] args) {
@@ -11,50 +15,81 @@ public class MVStoreDirect {
 
         MVMap<Integer, String> map = s.openMap("data");
 
-        int[] arr = new int[]{10000, 100000, 1000000, 10000000};
-        boolean random = true;
-        int maxVal = arr[arr.length - 1];
+        map.remove(1);
 
-
-//        map.keySet().toArray();
-
-        for (int i = 0; i < maxVal; i++) {
-            int randomNum = ThreadLocalRandom.current().nextInt(0, maxVal);
-            map.put(i, i + "");
-        }
-
+        int[] arr = new int[]{1000}; // array inex out of bound 100000000
+        boolean randomPut = false;
+        boolean randomGet = false;
 
         for (int lim : arr) {
+
             long init = currentTimeMillis();
 
-            for (int i = 0; i < lim + 1; i++) {
-                if (random) {
-                    int randomNum = ThreadLocalRandom.current().nextInt(0, maxVal);
-                    String sd = map.getInterpolate(randomNum);
-                } else {
-                    String sd = map.getInterpolate(i);
+            for (int i = 0; i < lim; i++) {
+                if(randomPut){
+                    int randomNum = ThreadLocalRandom.current().nextInt(0, lim);
+                    map.putInterpolate(randomNum, i + "");
+                }else {
+                    map.putInterpolate(i, i + "");
                 }
             }
-
             long end = currentTimeMillis();
-            System.out.print(lim + " -->  Interpolation: " + Long.toString(end - init) + "   ----    ");
+
+            System.out.print("Put "+lim + " -->  Interpolation: " + Long.toString(end - init) + "   ----    ");
+
+            init = currentTimeMillis();
+
+            for (int i = 0; i < lim; i++) {
+                if(randomPut){
+                    int randomNum = ThreadLocalRandom.current().nextInt(0, lim);
+                    map.put(randomNum, i + "");
+                }else {
+                    map.put(i, i + "");
+                }
+            }
+            end = currentTimeMillis();
+
+            System.out.println("Binary: " + Long.toString(end - init));
 
 
             init = currentTimeMillis();
 
             for (int i = 0; i < lim + 1; i++) {
-                if (random) {
-                    int randomNum = ThreadLocalRandom.current().nextInt(0, maxVal);
+                if (randomGet) {
+                    int randomNum = ThreadLocalRandom.current().nextInt(0, lim);
+                    String sd = map.getInterpolate(randomNum);
+                    //System.out.println(sd);
+                } else {
+                    String sd = map.getInterpolate(i);
+                    //System.out.println(sd);
+                }
+            }
+
+            end = currentTimeMillis();
+            System.out.print("Get "+lim + " -->  Interpolation: " + Long.toString(end - init) + "   ----    ");
+
+
+            init = currentTimeMillis();
+
+            for (int i = 0; i < lim + 1; i++) {
+                if (randomGet) {
+                    int randomNum = ThreadLocalRandom.current().nextInt(0, lim);
                     String sd = map.get(randomNum);
+                    //System.out.println(sd);
                 } else {
                     String sd = map.get(i);
+                    //System.out.println(sd);
                 }
             }
 
             end = currentTimeMillis();
             System.out.println("Binary: " + Long.toString(end - init));
-        }
 
-        s.close();
+            try {
+                Files.deleteIfExists(Paths.get("newfile"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
